d754a818cc22b0a39787fc8cc714428aea27eec7
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-05 21:48:03 +1000

    Update AI Controller

--- NavMeshPrac/Assets/AIController.cs
+++ NavMeshPrac/Assets/AIController.cs
@@ -6,16 +6,28 @@ using UnityEngine.AI;
 public class AIController : MonoBehaviour
 {
     public NavMeshAgent agent;
+    // Get hold of the animator on the player
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Test where player is to distination - to end walking animation
+        if (agent.remainingDistance < 2)
+        {
+            // Set animation for idle - isMoving string is from Animator on player, go to graph and in conditions (flag)
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetBool("isMoving", true);
+        }
     }
 }
--- NavMeshPrac/Assets/AgentManager.cs
+++ NavMeshPrac/Assets/AgentManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AgentManager : MonoBehaviour
 {
-    // Collect agent objects and keep in an array
-    GameObject[] agents;
+    // Collect agent objects and keep in an list
+    List<NavMeshAgent> agents = new List<NavMeshAgent>();
 
     // Start is called before the first frame update
     void Start()
     {
-        agents = GameObject.FindGameObjectsWithTag("AI");
+        GameObject[] a = GameObject.FindGameObjectsWithTag("AI");
+        foreach (GameObject go in a)
+        {
+            agents.Add(go.GetComponent<NavMeshAgent>());
+        }
     }
 
     // Update is called once per frame
@@ -22,9 +28,9 @@ public class AgentManager : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) // Distance of 100
             {
-                foreach (GameObject a in agents)
+                foreach (NavMeshAgent a in agents)
                 {
-                    a.GetComponent<AIController>().agent.SetDestination(hit.point);
+                    a.SetDestination(hit.point);
                 }
             }
         }
